1f989676641044492857d1b1cc704930bb168e83
El Hadji <elhadji.sarr@outlook.com>
2026-03-23 21:23:54 +0100

    day 2

--- net_inspect.sh
+++ net_inspect.sh
@@ -5,33 +5,35 @@ echo "== NETWORK INSPECTION =="
 echo "IP ADDRESS: $(hostname -I)"
 
 
-if [[ $(ping -c 3 -q google.com) == *"3 received, 0% packet loss,"* ]]; then
+if ping -c 3 -q 8.8.8.8 > /dev/null 2>&1; then
     echo "✓ Internet reachable"
 else
     echo "✗ No internet"
 fi
 
-echo "DNS LOOKUP"
-echo $(nslookup github.com)
+echo "DNS LOOKUP for github.com"
+nslookup github.com | grep "Address" | tail -1
+echo ""
  
-if [[ $(ss -tulpn | grep 127.0.0.1:22) ]]; then
-    echo "Port 22 is onpen on localhost"
+if ss -tulpn | grep -q ":22 "; then
+    echo "Port 22 is onpen"
 else
     echo "Port 22 is closed"
 fi 
 
-if [[ $(ss -tulpn | grep 127.0.0.1:80) ]]; then
-    echo "Port 80 is open on localhost"
+if ss -tulpn | grep -q ":80 "; then
+    echo "Port 80 is open"
 else
     echo "Port 80 is closed"
 fi
 
 echo "Status code on exemple.com: $(curl -s -o /dev/null -w "%{http_code}" https://example.com)"
 
-if [[ $(sudo ufw status) == "Status: inactive" ]]; then
+if sudo ufw status |  grep -q "Status: inactive"; then
     echo "ufw inactive"
 else
     echo "ufw active"
 fi
+
 echo "== END OF REPORT =="
 
